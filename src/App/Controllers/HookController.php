6c33cea29b273a944b72e3dd1c1859e9a1931bb2
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2023-12-26 10:32:55 +0200

    fix mime-type of audio

--- src/App/Controllers/HookController.php
+++ src/App/Controllers/HookController.php
@@ -20,13 +20,25 @@ readonly class HookController
 //        token: 2X2r4Rp51W2HYSJfDn6RFjZY45h_2F_j372fRgyMav_uUjfN7m5vlJBi3g8G_Miz
         $ticket_article = $this->client->resource(ResourceType::TICKET_ARTICLE);
 
+
+
+
         $attachments = [];
-        foreach ($_FILES as $file) {
+        foreach ($_FILES as $key => $file) {
             $fileContent = base64_encode(file_get_contents($file['tmp_name']));
+
+            $mime_type = mime_content_type($file['tmp_name']);
+
+            $filename = $file['name'];
+            if (str_contains($key, "voice_message")) {
+                $filename = $key;
+            }
+
             $attachment = [
-                'filename' => $file['name'],
+                'filename' => $filename,
                 'data' => $fileContent,
-                'mime-type' => 'text/plain'
+                'mime-type' => $mime_type,
+                'type' => 'phone'
             ];
             $attachments[] = $attachment;
         }
